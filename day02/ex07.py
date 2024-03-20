d702276cabbc63760bdd06e7a167a3f93157c19a
ERICDDDDSSS <v5623@naver.com>
2024-03-20 21:25:36 +0900

    파이썬 실습코드

---
+++ day02/ex07.py
@@ -0,0 +1,8 @@
+name = '이승섭'
+print('내 이름은 %s 입니다.' % name)
+height = 189
+print('내 키는 %.2f cm 입니다.' % height)
+weight = 89
+print('내 몸무게는 %.2f kg 입니다.' % weight)
+year,month,day = 2000,7,16
+print('내 생일은 %d년 %d월 %d일 입니다.' %(year,month,day))
---
+++ day02/ex12.py
@@ -0,0 +1,13 @@
+# 멤버쉼 연산자
+# a in c : 컬렉션 c 요소에 a 가 포함되어 있으면 True
+#          그렇지않으면 False
+
+a = [1,2,3]
+
+x = 3 in a
+y = 10 in a
+z = 100 not in a
+
+print('x : {}'.format(x))
+print('x : {}'.format(y))
+print('x : {}'.format(z))
